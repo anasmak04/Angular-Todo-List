34a46808240150024d703551f9ddaa11e6aa404d
anasmak04 <anasdev8@gmail.com>
2022-09-08 18:15:41 +0200

    add event add and remove

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todo-list';
+  list: any[] = [];
+  addTask(item: String) {
+    this.list.push({ id: this.list.length, name: item });
+    console.log(this.list);
+  }
+
+  RemoveTask(id:number){
+    console.warn("remove")
+    this.list=this.list.filter(item => item.id !== id);
+  }
 }
