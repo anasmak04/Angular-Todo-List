016755e568dbe703e6f987246fde6964f4a94134
anasmak04 <anasdev8@gmail.com>
2022-09-08 20:51:18 +0200

    add material Anguar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
