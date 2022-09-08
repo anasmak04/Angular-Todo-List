bad376b6b5034fa5ced479c23f990dd7197a6103
anasmak04 <anasdev8@gmail.com>
2022-09-08 21:33:49 +0200

    add some styles

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -3,22 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  list: any[] = [];
+  addTask(item: String) {
+    this.list.push({ id: this.list.length, name: item });
   }
 
-  list: any[] =[];
-  addTask(item:String){
-    this.list.push({id:this.list.length,name:item});
-  } 
-
-  RemoveTask(id:number){
-    this.list=this.list.filter(item => item.id !== id);
+  RemoveTask(id: number) {
+    this.list = this.list.filter((item) => item.id !== id);
   }
-
 }
